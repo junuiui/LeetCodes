1c345f85ee5e2569c0158f6c3ffb4399c51978e7
Junui Hong <jhong980821@outlook.com>
2025-05-28 13:32:14 -0700

    58 added

--- c++/Easy/58_length_of_last_word.cpp
+++ c++/Easy/58_length_of_last_word.cpp
@@ -1,6 +1,28 @@
+#include <string>
+
+using namespace std;
+
 class Solution {
 public:
     int lengthOfLastWord(string s) {
         
+        // going backward
+        int len = s.length();
+        int i = len - 1;
+        int result = 0;
+
+        // setting up the last starting index
+        while (s[i] == ' '){
+            i--;
+        }
+
+        while (i >= 0) {
+            if (s.at(i) == ' ') break;
+            else {
+                result++;
+                i--;
+            }
+        }
+        return result;
     }
 };
