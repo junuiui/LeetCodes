45537409d8cc0f955f638a1838076d38eb309679
Junui Hong <jhong980821@outlook.com>
2025-05-30 12:22:31 -0700

    69 solution aded

--- c++/Easy/69_sqrt(x).cpp
+++ c++/Easy/69_sqrt(x).cpp
@@ -1,6 +1,26 @@
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
 class Solution {
 public:
     int mySqrt(int x) {
-        
+        if (x == 0) return 0;
+        if (x == 1) return 1;
+
+        int lower = 1;
+        int upper = x;
+        int mid = -1;
+
+        while (lower <= upper) {
+            mid = lower + (upper - lower) / 2;
+            long long square = static_cast<long long>(mid) * mid;
+            if (square > x) upper = mid -1;
+            else if (square == x) return mid;
+            else lower = mid + 1;
+        }
+
+        return std::round(upper);
     }
 };
+// 1 4 9 16
