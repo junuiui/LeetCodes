63a78bc20da67cf44ca9b5d69086ffab9e9dcb87
Junui Hong <jhong980821@outlook.com>
2025-06-05 10:48:08 -0700

    925 sol added

--- c++/Easy/925_long_pressed.cpp
+++ c++/Easy/925_long_pressed.cpp
@@ -6,7 +6,20 @@ using namespace std;
 class Solution {
 public:
     bool isLongPressedName(string name, string typed) {
+
+        int typedLength = typed.length();
+        int nameLength = name.length();
+
+        if (nameLength == 0 || typedLength < nameLength) return false;
+
+        int i = 0;
+
+        for (int j = 0; j < typedLength; j++) {
+            if (i < nameLength && name.at(i) == typed.at(j)) i++;
+            else if (j == 0 || typed.at(j) != typed.at(j-1)) return false;
+        }
         
+        return i == nameLength;
     }
 };
 
