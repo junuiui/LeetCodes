f001757611a61d7dc914cb2274f1f9ed37332d27
Junui Hong <jhong980821@outlook.com>
2025-06-10 12:18:34 -0700

    4ms

--- c++/Med/2_add_two_numbers.cpp
+++ c++/Med/2_add_two_numbers.cpp
@@ -11,6 +11,34 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+        // List node is non empty
+
+        ListNode* preHead = new ListNode(0);
+        ListNode* curr = preHead;
+
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry != 0){
+
+            int sum = carry;
+
+            if (l1 != nullptr){
+                sum += l1->val;
+                l1 = l1->next;
+            } 
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+        }
+
+        ListNode* result = preHead->next;
+        delete preHead;
+
+        return result;
     }
 };
