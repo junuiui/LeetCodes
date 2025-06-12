1be23599b71f140001a6c6da9e2699913766761a
Junui Hong <jhong980821@outlook.com>
2025-06-12 14:17:02 -0700

    sol added

--- c++/Easy/88_merge_sorted_array.cpp
+++ c++/Easy/88_merge_sorted_array.cpp
@@ -6,7 +6,37 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
-        
+        /*
+            nums1.length == m + n
+            nums2.length == n
+            0 <= m, n <= 200
+            1 <= m + n <= 200
+            -10^9 <= nums1[i], nums2[j] <= 10^9
+        */
 
+        if (n == 0) return;
+
+        if (m == 0){
+            for (int i = 0; i < m+n; i++){
+                nums1[i] = nums2[i];
+            }
+        }
+
+        int i = m - 1; // for m
+        int j = n - 1; // for n
+        int curr = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]){
+                nums1[curr--] = nums1[i--];
+            }
+            else{
+                nums1[curr--] = nums2[j--];
+            }
+        }
+
+        while (j>=0){
+            nums1[curr--] = nums2[j--];
+        }
     }
 };
