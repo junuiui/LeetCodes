8696ae15d532b7ab50555c127d894ff248b22f71
Junui Hong <jhong980821@outlook.com>
2025-06-11 19:56:26 -0700

    8 sol added

--- c++/Med/8_string_to_int.cpp
+++ c++/Med/8_string_to_int.cpp
@@ -1,13 +1,41 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     int myAtoi(string s) {
-        int s_len = s.length();
+        int pointer = 0;
+        int sLen = s.length();
+        long long result = 0;
 
-        
+        bool isNeg = false;
+
+        // leading whitespaces
+        while (s[pointer] == ' ' && pointer < sLen) {
+            pointer++;
+        }
+
+        // + / -
+        if (pointer < sLen && (s[pointer] == '-' || s[pointer] == '+')){
+            isNeg = (s[pointer] == '-');
+            pointer++;
+        }
+
+        while (pointer < sLen && isdigit(s[pointer])){
+            int digit = s[pointer] - '0';
+
+            if (result > (INT_MAX - digit) / 10) {
+                return isNeg ? INT_MIN : INT_MAX;
+            }
+
+            result = result * 10 + digit;
+
+            pointer++;
+        }
+
+        return isNeg ? -result : result;
     }
 };
