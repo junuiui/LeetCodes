5532f92dca0e4f656f24f047728c4f283b07b46c
junuiui <jhong980821@outlook.com>
2025-06-18 13:42:15 -0700

    217 added

---
+++ c++/Arrays & Hashing/E217_contains_duplicates.cpp
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/contains-duplicate/description/
+
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+
+using namespace std;
+
+// // Sort Version
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+
+//         int numsLength = nums.size();
+
+//         if (numsLength == 0) return false;
+        
+//         // sort? O(NlogN)
+//         sort(nums.begin(), nums.end());
+
+//         int target = nums[0];
+//         int k;
+//         for (int i = 1; i < numsLength; i++) {
+//             k = nums[i];
+//             if (target == k) return true;
+//             else target = k;
+//         }
+//         return false;
+//     }
+// };
+
+// Hash Map?
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        unordered_set<int> seen;
+
+        for (int k : nums){
+            if (seen.count(k)) return true;
+            seen.insert(k);
+        }
+
+        return false;
+    }
+};
