baa5d1198b659f98ca9a7c071bbcf68ee80017fe
junuiui <jhong980821@outlook.com>
2025-06-19 10:56:15 -0700

    sol added

--- c++/Arrays & Hashing/M347_top_k_freq_elements.cpp
+++ c++/Arrays & Hashing/M347_top_k_freq_elements.cpp
@@ -1,10 +1,23 @@
 #include <vector>
+#include <unordered_map>
+#include <iostream>
 
 using namespace std;
 
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        unordered_map<int, int> freq;
         
+        for (int i : nums) {
+            freq[i]++;
+        }
+
+        for (auto& [num, count] : freq) {
+            cout << num << " appears " << count << " times" << endl;
+        }
+
+        return {0};
     }
 };
